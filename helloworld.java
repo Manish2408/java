77312334f9609365143009c5fd1b263433e26b2b
Manish kumar <manishrlmra123@gmail.com>
2024-10-21 22:05:49 +0530

    changes in code

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+class Simple{
+    public static void main(String args[]){
+    
+    System.out.println("Welcome to Cloud knowledge ");
+    
     }
-}
+    }
